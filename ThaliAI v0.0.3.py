da7cf2e8e9eece952b42d379aa994a530e49f766
devishjack <devishjack@gmail.com>
2023-12-28 19:35:15 -0600

    added visual of thalia

--- ThaliAI v0.0.3.py
+++ ThaliAI v0.0.3.py
@@ -3,14 +3,23 @@ import speech_recognition as sr
 import time
 import elevenlabs
 from elevenlabs import set_api_key
+import os
+from tkinter import *
+from PIL import ImageTk, Image
 
 listener = sr.Recognizer()
+root = Tk()
+root.title('ThaliAI')
+root.iconbitmap('ThaliaExpressions\\ThaliAIIcon.ico')
+my_img = ImageTk.PhotoImage(Image.open('ThaliaExpressions\\ThaliaNorm.png'))
+my_label = Label(image=my_img)
 
-#enter your elevenlabs API key here
-elevenAPI = ''
+happywords = ['happy','thrilled','excited','excitment']
+sarcasticwords = ['sarcastic','sarcasm']
 
-#enter your openai API key here
-openAPI = ''
+OpenAPI = ''
+ElevenAPI = ''
+
 
 
 def take_command():
@@ -25,11 +34,35 @@ def take_command():
             print(e)
     return command
 
+def change_image(emo):
+    global my_img
+    global my_label
+
+    my_label.grid_forget()
+    my_img = ImageTk.PhotoImage(Image.open('ThaliaExpressions\\' + emo + '.png'))
+    my_label = Label(image=my_img)
+    my_label.grid(row=0,column=0,columnspan=3)
+    root.update()
+
 def talk(text):
     audio = elevenlabs.generate (
         text = text,
         voice = "Dorothy",
     )
+    for word in happywords:
+        if word in text:
+            print('changing expression happy')
+            change_image('ThaliaHappy')
+        else:
+            print('changing expression norm')
+            change_image('ThaliaNorm')
+    for word in sarcasticwords:
+        if word in text:
+            print('changing expression annoyed')
+            change_image('ThaliaAnnoyed')
+        else:
+            print('changing expression norm')
+            change_image('ThaliaNorm')
     elevenlabs.play(audio)
 
 def generate_response(client, prompt, thread):
@@ -57,8 +90,10 @@ def generate_response(client, prompt, thread):
              return (mesg.content[0].text.value)
 
 def main():
-        set_api_key(api_key=elevenAPI)
-        client = openai.OpenAI(api_key = openAPI)
+        my_label.grid(row=0,column=0,columnspan=3)
+        root.update()
+        set_api_key(api_key=ElevenAPI)
+        client = openai.OpenAI(api_key=OpenAPI)
         thread = client.beta.threads.create()
         while True:
             command = take_command()
@@ -76,7 +111,7 @@ def main():
                         talk(response)
             elif 'quit' in command:
                 break
-
+ 
 if __name__ == "__main__":
    main()
 
